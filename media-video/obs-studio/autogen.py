4a6b9ee1aeeba85d84ad92f14c5d974b40252c82
Daniel Robbins <drobbins@funtoo.org>
2022-10-07 23:51:51 -0600

    Autogenerated tree updates.
    merged:
    
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 016c6922e2188c0b0f20107b482ceff3276a7b8c
      deadbeef: 53e0a41db6e7193bd83ef117f8055457f0990cb7
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 0b4e59372ec4a68106da6be82991054d4aff58bb

--- media-video/obs-studio/autogen.py
+++ media-video/obs-studio/autogen.py
@@ -3,10 +3,46 @@
 import json
 from re import match
 
+async def get_plugin_info(user, repo, select='^\d+\.\d+\.\d+$'):
+	module_info={}
+	tags = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/tags",
+		is_json = True
+	)
+
+#	res=httpx.get(f"https://api.github.com/repos/{user}/{repo}/tags")
+#	tags = json.loads(res.content)
+
+	selected = None
+	for tag in tags:
+		selected = (
+			tag['name'] 
+				if match(select, tag['name']) != None 
+			else None
+		)
+		if selected:
+			version = selected
+			url = tag['tarball_url']
+			break
+	if selected is None:
+		module_info = None
+		raise KeyError(f"Unable to find suitable release/tag for {repo}.")
+	else:
+		module_info['user'] = user
+		module_info['repo'] = repo
+		module_info['select'] = select
+		module_info['version'] = version
+		module_info['url'] = url
+		module_info['final_name']=f"{repo}-{version}.tar.gz"
+		module_info['atifact'] = hub.pkgtools.ebuild.Artifact(url=url)
+	return module_info
+
 
 async def generate(hub, **pkginfo):
 	github_user = "obsproject"
 	github_repo = "obs-studio"
+	artifacts = []
+
 	json_list = await hub.pkgtools.fetch.get_page(
 		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
 	)
@@ -19,26 +55,38 @@ async def generate(hub, **pkginfo):
 		url = release["tarball_url"]
 		break
 	final_name = f'{pkginfo["name"]}-{version}.tar.gz'
-	src_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
-	browser_repo = "obs-browser"
-	browser_commits = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{browser_repo}/commits", is_json=True
-	)
-	browser_target_commit = browser_commits[0]
-	browser_target_hash = browser_target_commit["sha"]
-	browser_url = f"https://github.com/{github_user}/{browser_repo}/archive/{browser_target_hash}.tar.gz"
-	browser_final_name = f"{browser_repo}-{browser_target_hash}.tar.gz"
-	browser_artifact = hub.pkgtools.ebuild.Artifact(url=browser_url)
+	artifacts.append( hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name) )
+
 	cef_dir = "cef_binary_4280_linux64"
 	cef_url = f"https://cdn-fastly.obsproject.com/downloads/{cef_dir}.tar.bz2"
-	cef_artifact = hub.pkgtools.ebuild.Artifact(url=cef_url)
+	artifacts.append( hub.pkgtools.ebuild.Artifact(url=cef_url) )
+
+	# The Browser repo has version tags and release assets. Let's get the latest ones.
+
+	plugins = []
+	for plugin in ['obs-browser','obs-websocket','obs-amd-encoder']:
+		plugins.append( await get_plugin_info(github_user, plugin) )
+
+	for plugin in plugins:
+		artifacts.append(
+			hub.pkgtools.ebuild.Artifact(
+				url=plugin['url'], 
+				final_name=plugin['final_name']
+			)
+		)
+
+	fixed_version="27.2.4"
+	print (f"The latest version is {version}, but currently it won't build.\nVersion is fixed to {fixed_version}")
+	version=fixed_version
+
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		github_user=github_user,
 		github_repo=github_repo,
 		version=version,
 		cef_dir=cef_dir,
-		artifacts=[src_artifact, browser_artifact, cef_artifact],
+		plugins=plugins,
+		artifacts=artifacts,
 	)
 	ebuild.push()
 
